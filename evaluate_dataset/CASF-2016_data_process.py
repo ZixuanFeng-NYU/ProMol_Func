b7a5d9b67b1a3f5b92adb2b9fa48da8b28c4f5a6
Zixuan Feng_NYU <76710319+ZixuanFeng-NYU@users.noreply.github.com>
2024-09-10 19:29:17 -0400

    Update CASF-2016_data_process.py

--- evaluate_dataset/CASF-2016_data_process.py
+++ evaluate_dataset/CASF-2016_data_process.py
@@ -1,3 +1,10 @@
+## convert ligands mol2 to smiles
+import os
+
+pdb_id_list=os.listdir("CASF-2016/coreset")
+for pdb_id in pdb_id_list:
+    os.system(f"obabel -imol2 CASF-2016/coreset/{pdb_id}/{pdb_id}_ligand.mol2 -osmi -O CASF-2016/coreset/{pdb_id}/{pdb_id}_ligand.smi")
+
 ## convert pdb to fasta file
 import os
 import subprocess
