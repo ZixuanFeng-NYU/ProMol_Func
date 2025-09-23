ab5d8359eb3609ebb1bc356e8108d5059c7609d8
Zixuan Feng_NYU <76710319+ZixuanFeng-NYU@users.noreply.github.com>
2025-09-22 21:12:45 -0400

    modify make_predictions_add.py

--- KANO/chemprop/train/make_predictions_add.py
+++ KANO/chemprop/train/make_predictions_add.py
@@ -96,66 +96,7 @@ def make_predictions(args: Namespace, smiles: List[str] = None) -> List[Optional
     return avg_preds, test_data.smiles(), test_data.pro_id(),test_data.targets()
 
 
-def get_embs(args: Namespace, smiles: List[str] = None) -> List[Optional[List[float]]]:
-    """
-    Makes predictions. If smiles is provided, makes predictions on smiles. Otherwise makes predictions on args.test_data.
 
-    :param args: Arguments.
-    :param smiles: Smiles to make predictions on.
-    :return: A list of lists of target predictions.
-    """
-    if args.gpu is not None:
-        torch.cuda.set_device(args.gpu)
-
-    print('Loading data')
-    if smiles is not None:
-        test_data = get_data_from_smiles(smiles=smiles, skip_invalid_smiles=False)
-    else:
-        test_data = get_data(path=args.test_path, args=args, use_compound_names=args.use_compound_names, skip_invalid_smiles=False)
-
-    print('Validating SMILES')
-    valid_indices = [i for i in range(len(test_data)) if test_data[i].mol is not None]
-    full_data = test_data
-    test_data = MoleculeDataset([test_data[i] for i in valid_indices])
-
-    # Edge case if empty list of smiles is provided
-    if len(test_data) == 0:
-        return [None] * len(full_data)
-
-    if args.use_compound_names:
-        compound_names = test_data.compound_names()
-    print(f'Test size = {len(test_data):,}')
-
-
-    if args.checkpoint_path is not None:
-        print(f'Predicting with an ensemble of {len(args.checkpoint_paths)} models')
-        for checkpoint_path in tqdm(args.checkpoint_paths, total=len(args.checkpoint_paths)):
-            # Load model
-            model = load_checkpoint(checkpoint_path, cuda=args.cuda)
-            print(model)
-            # model = build_pretrain_model(args, encoder_name=args.encoder_name)
-            #model.encoder.load_state_dict(torch.load(checkpoint_path))
-
-            model = model.cuda()
-
-            model_preds = get_emb(
-                model=model,
-                prompt=False,
-                data=test_data,
-                batch_size=args.batch_size,
-                scaler=None
-            )
-    else:
-        model = build_pretrain_model(args, encoder_name=args.encoder_name)
-        model = model.cuda()
-
-        model_preds = get_emb(
-            model=model,
-            prompt=False,
-            data=test_data,
-            batch_size=args.batch_size,
-            scaler=None
-        )
 
     return model_preds, test_data.smiles(),test_data.pro_id(),test_data.targets()
 
