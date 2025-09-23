a80cba76ba00557d72a2aae40b352adf157ca5d5
Zixuan Feng_NYU <76710319+ZixuanFeng-NYU@users.noreply.github.com>
2025-09-22 21:21:29 -0400

    Refactor imports in __init__.py

--- KANO/chemprop/train/__init__.py
+++ KANO/chemprop/train/__init__.py
@@ -1,5 +1,5 @@
 from .evaluate_with_add import evaluate, evaluate_predictions
-from .predict_with_add import predict, get_emb
-from .run_training_with_add import run_training, pre_training
+from .predict_with_add import predict
+from .run_training_with_add import run_training
 from .train_with_add import train
 from .make_predictions_add import make_predictions
